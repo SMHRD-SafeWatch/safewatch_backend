aa06137ac38c73e36ffe764d6d1768a6ec48aa12
HanJaeOk <fpalffldk09@naver.com>
2024-11-13 17:45:39 +0900

    비교 수정

--- src/main/java/com/sw/sw/Sw2Application.java
+++ src/main/java/com/sw/sw/Sw2Application.java
@@ -2,12 +2,15 @@ package com.sw.sw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class TestApplication {
+@EnableScheduling
+public class Sw2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+
+		SpringApplication.run(Sw2Application.class, args);
 	}
 
 }
