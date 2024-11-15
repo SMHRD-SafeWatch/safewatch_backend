a48cfbe2fdb95565d2f388f814dfa29359bb29cf
2totwo <2todeux@gmail.com>
2024-11-15 14:47:40 +0900

    education 일정 등록, 삭제 기능 추가

--- src/main/java/com/sw/sw/service/EducationService.java
+++ src/main/java/com/sw/sw/service/EducationService.java
@@ -3,8 +3,10 @@ package com.sw.sw.service;
 import com.sw.sw.entity.Education;
 import com.sw.sw.repository.EducationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 
@@ -24,6 +26,23 @@ public class EducationService {
             educations.sort(Comparator.comparing(Education::getEduDate).reversed());
         }
         return educations;
+    }
+
+    // 일정 등록
+    public void addEducation(Education education) {
+        educationRepository.save(education);
+    }
+
+    // 일정 삭제
+    public void deleteEducationById(int eduId) {
+        educationRepository.deleteById(eduId);
+    }
+
+    // 과거 일정 자동 삭제 (매일 자정 실행)
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void deletePastEducations() {
+        LocalDateTime now = LocalDateTime.now();
+        educationRepository.deleteByEduDateBefore(now);
     }
 
 }
