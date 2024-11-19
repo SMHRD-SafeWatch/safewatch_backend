85d260743aa357308fbf0340a4e20de96c649f8f
2totwo <2todeux@gmail.com>
2024-11-19 15:45:28 +0900

    back-end 통합

--- src/main/java/com/sw/sw/controller/EducationController.java
+++ src/main/java/com/sw/sw/controller/EducationController.java
@@ -3,9 +3,14 @@ package com.sw.sw.controller;
 import com.sw.sw.entity.Education;
 import com.sw.sw.service.EducationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,5 +28,25 @@ public class EducationController {
         return "analytics";
     }
 
-}
+    // 일정 등록
+    @PostMapping("/education")
+    public ResponseEntity<String> addEducation(@RequestBody Education education) {
+        try {
+            educationService.addEducation(education);
+            return new ResponseEntity<>("Education registered successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error registering education", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    // 일정 삭제
+    @PostMapping("/deleteEducation")
+    public ResponseEntity<Void> deleteEducation(@RequestParam("eduId") int eduId) {
+        try {
+            educationService.deleteEducationById(eduId);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
